a3dc4a146e7ed22cf04f6180d81557602f909ebd
Ysuarez93 <yordy_9328@gmail.com>
2025-06-27 19:10:34 -0500

    base de datos

--- Database.sql
+++ Database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-06-2025 a las 05:57:24
+-- Tiempo de generación: 28-06-2025 a las 02:08:43
 -- Versión del servidor: 11.7.2-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,60 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `objetosperdidos`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comprobante`
+--
+
+CREATE TABLE `comprobante` (
+  `ID_comprobante` int(11) NOT NULL,
+  `ID_objeto` int(11) DEFAULT NULL,
+  `ID_usuario1` int(11) DEFAULT NULL,
+  `ID_usuario2` int(11) DEFAULT NULL,
+  `Fecha_de_entrega` date DEFAULT NULL,
+  `Fecha_de_recibimiento` date DEFAULT NULL,
+  `Descripcion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado`
+--
+
+CREATE TABLE `estado` (
+  `ID_estado` int(11) NOT NULL,
+  `Estado` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inicio_sesion`
+--
+
+CREATE TABLE `inicio_sesion` (
+  `ID_Inicio_Sesion` int(11) NOT NULL,
+  `Nombre_de_Usuario` varchar(50) NOT NULL,
+  `Contraseña` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `objetos`
+--
+
+CREATE TABLE `objetos` (
+  `ID_Objeto` int(11) NOT NULL,
+  `ID_estado` int(11) DEFAULT NULL,
+  `Objeto` varchar(100) NOT NULL,
+  `Ubicacion` varchar(100) DEFAULT NULL,
+  `Descripcion` text DEFAULT NULL,
+  `Imagen` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -45,17 +99,142 @@ CREATE TABLE `objetos_perdidos` (
 --
 
 INSERT INTO `objetos_perdidos` (`id`, `categoria`, `nombre`, `color`, `tamaño`, `descripcion`, `imagen_ruta`, `fecha_registro`, `tipo`, `estado`) VALUES
-(1, 'tecnología', 'Telefono Iphone 15', 'Azul', 'No aplica', 'Se ha encontrado telefono en las inmediaciones de la cafeteria a las 12 pm', '../img/publicaciones/publicacion-1.jpg', '2025-06-04 10:37:50', 'Objeto pérdido', 'activo');
+(1, 'tecnología', 'Telefono Iphone 15', 'Azul', 'No aplica', 'Se ha encontrado telefono en las inmediaciones de la cafeteria a las 12 pm', '../../img/publicaciones/publicacion-1.jpg', '2025-06-04 10:37:50', 'Objeto pérdido', 'activo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `ID_rol` int(11) NOT NULL,
+  `Nombre_rol` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`ID_rol`, `Nombre_rol`) VALUES
+(1, 'RUT'),
+(2, 'Usuario');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `ID_usuario` int(11) NOT NULL,
+  `ID_rol` int(11) DEFAULT NULL,
+  `ID_objeto` int(11) DEFAULT NULL,
+  `Nombre_de_Usuario` varchar(50) NOT NULL,
+  `Correo` varchar(100) NOT NULL,
+  `Contraseña` varchar(100) NOT NULL,
+  `Direccion` varchar(255) DEFAULT NULL,
+  `Fecha_de_Nacimiento` date DEFAULT NULL,
+  `fecha_registro` date NOT NULL,
+  `Telefono` varchar(15) DEFAULT NULL,
+  `imagen_perfil` varchar(50) NOT NULL,
+  `estado` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`ID_usuario`, `ID_rol`, `ID_objeto`, `Nombre_de_Usuario`, `Correo`, `Contraseña`, `Direccion`, `Fecha_de_Nacimiento`, `fecha_registro`, `Telefono`, `imagen_perfil`, `estado`) VALUES
+(1026577616, 2, NULL, 'Yordy Suarez', 'yordy_9328@hotmail.com', '1026577616', 'CL 72 SUR 22 26', '2025-06-13', '2025-06-14', '3214996400', '../img/perfil/predeterminado.jpg', 'activo');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `comprobante`
+--
+ALTER TABLE `comprobante`
+  ADD PRIMARY KEY (`ID_comprobante`),
+  ADD KEY `ID_objeto` (`ID_objeto`),
+  ADD KEY `ID_usuario1` (`ID_usuario1`),
+  ADD KEY `ID_usuario2` (`ID_usuario2`);
+
+--
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`ID_estado`);
+
+--
+-- Indices de la tabla `inicio_sesion`
+--
+ALTER TABLE `inicio_sesion`
+  ADD PRIMARY KEY (`ID_Inicio_Sesion`),
+  ADD UNIQUE KEY `Nombre_de_Usuario` (`Nombre_de_Usuario`);
+
+--
+-- Indices de la tabla `objetos`
+--
+ALTER TABLE `objetos`
+  ADD PRIMARY KEY (`ID_Objeto`),
+  ADD KEY `ID_estado` (`ID_estado`);
+
+--
 -- Indices de la tabla `objetos_perdidos`
 --
 ALTER TABLE `objetos_perdidos`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`ID_rol`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`ID_usuario`),
+  ADD UNIQUE KEY `Nombre_de_Usuario` (`Nombre_de_Usuario`),
+  ADD UNIQUE KEY `Correo` (`Correo`),
+  ADD KEY `ID_rol` (`ID_rol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comprobante`
+--
+ALTER TABLE `comprobante`
+  MODIFY `ID_comprobante` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `ID_estado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `inicio_sesion`
+--
+ALTER TABLE `inicio_sesion`
+  MODIFY `ID_Inicio_Sesion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `objetos`
+--
+ALTER TABLE `objetos`
+  MODIFY `ID_Objeto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `ID_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1026577619;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
